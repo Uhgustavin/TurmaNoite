04d8c2b06038593124af0b3ae5d40de70c544738
Uhgustavin <79878374+Uhgustavin@users.noreply.github.com>
2021-03-03 19:56:03 -0300

    Add files via upload

---
+++ ProjAlun/src/projalun/ProjAlun.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projalun;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class ProjAlun {
+
+
+        public static void main
+        (String[] args) {String nomeDoAluno ;
+        Scanner ler = new Scanner (System.in);
+        double nota1;
+        double nota2;
+        double nota3;
+        
+        
+        
+     
+                System.out.println("digite o nome do aluno");
+                
+                nomeDoAluno = ler.nextLine();
+                System.out.println(nomeDoAluno);
+               
+        // TODO code application logic here
+    }
+        
+    
+}
